2678e67651580717653d0a1cbaae25bc1ea822a0
galego-vinicius <vfelix1998@gmail.com>
2026-06-05 15:32:57 -0300

    fix: adjust unit tests app module spec

--- test/unit/app.module.spec.ts
+++ test/unit/app.module.spec.ts
@@ -1,5 +1,21 @@
 import { AppModule } from '../../src/app.module';
 
+jest.mock('@nestjs/typeorm', () => {
+  const actual = jest.requireActual('@nestjs/typeorm');
+  return {
+    ...actual,
+    TypeOrmModule: {
+      ...actual.TypeOrmModule,
+      forRootAsync: jest.fn().mockReturnValue({ module: class TypeOrmRootModule {} }),
+      forFeature: jest.fn().mockReturnValue({ module: class TypeOrmFeatureModule {} }),
+    },
+  };
+});
+
+jest.mock('@shared/infrastructure/database/typeorm.config', () => ({
+  buildTypeOrmOptions: jest.fn().mockReturnValue({}),
+}));
+
 describe('AppModule', () => {
   it('should be defined', () => {
     expect(AppModule).toBeDefined();
