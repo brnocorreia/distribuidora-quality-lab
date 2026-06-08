8c982407115135d573d20c5bb0387a3a26394385
ribmarciojr <marciojunior2109@gmail.com>
2026-06-08 12:47:43 -0300

    🔧 chore(pedido): add dedicated jest config and coverage scripts for order module

---
+++ jest.order.config.js
@@ -0,0 +1,14 @@
+/** @type {import('jest').Config} */
+module.exports = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: '.',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: { '^.+\\.(t|j)s$': 'ts-jest' },
+  collectCoverageFrom: ['src/modules/order/**/*.(t|j)s'],
+  coverageDirectory: './coverage',
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^@shared/(.*)$': '<rootDir>/src/shared/$1',
+    '^@modules/(.*)$': '<rootDir>/src/modules/$1',
+  },
+};
