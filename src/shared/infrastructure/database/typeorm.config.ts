2f7928318b917b7e02987978eb8ef2f955ad5f10
hmigl <88746406+hmigl@users.noreply.github.com>
2026-07-11 16:52:37 -0300

    🐛 fix(database): load module entities in TypeORM config

--- src/shared/infrastructure/database/typeorm.config.ts
+++ src/shared/infrastructure/database/typeorm.config.ts
@@ -16,8 +16,8 @@ export function buildTypeOrmOptions(params: TypeOrmConfigParams): DataSourceOpti
     migrationsRun: true,
     migrations: [__dirname + '/migrations/*{.ts,.js}'],
     entities: [
-      __dirname + '/../../**/*.aggregate{.ts,.js}',
-      __dirname + '/../../**/*.entity{.ts,.js}',
+      __dirname + '/../../../**/*.aggregate{.ts,.js}',
+      __dirname + '/../../../**/*.entity{.ts,.js}',
     ],
     logging: nodeEnv === 'development' ? ['error', 'migration'] : ['error'],
   };
